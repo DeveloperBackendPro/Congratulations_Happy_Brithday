927aeb4269ba2916552e64cf84ef24a87bb72fc3
IKROMOV DILSHOD <77174755+DeveloperBackendPro@users.noreply.github.com>
2022-04-14 02:53:37 +0500

    Update settings.py

--- Congratulations_Happy_Brithday/settings.py
+++ Congratulations_Happy_Brithday/settings.py
@@ -119,8 +119,8 @@ CKEDITOR_CONFIGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'leta19922@gmail.com'
-EMAIL_HOST_PASSWORD = 'ywnjivtoludnojcu'
+EMAIL_HOST_USER = 'GMAIL'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
 DEFAULT_FROM_EMAIL='PyWisher <no-reply@pywisher.ml>'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
